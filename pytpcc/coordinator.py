7ed4428a8339e6d97917ed5a48361fed74a5aef2
Andy Art <v1k1nghawk@users.noreply.github.com>
2023-03-20 15:34:14 +0100

    Fix joint setting of '--reset' and '--no-load' args

--- pytpcc/coordinator.py
+++ pytpcc/coordinator.py
@@ -145,7 +145,11 @@ if __name__ == '__main__':
     args = vars(aparser.parse_args())
 
     if args['debug']: logging.getLogger().setLevel(logging.DEBUG)
-        
+
+    ## Arguments validation
+    assert args['reset'] == False or args['no_load'] == False, \
+        "'--reset' and '--no-load' are incompatible with each other"
+
     ## Create a handle to the target client driver
     driverClass = createDriverClass(args['system'])
     assert driverClass != None, "Failed to find '%s' class" % args['system']
